955ce608af989d5a4063631b86a783fa90ddd0c8
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-10-17 13:42:02 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -139,5 +139,3 @@ svn co https://github.com/DHDAXCW/packages/trunk/utils/coremark customfeeds/pack
 # 风扇脚本
 wget -P target/linux/rockchip/armv8/base-files/etc/init.d/ https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux/rockchip-rk3328/base-files/etc/init.d/fa-rk3328-pwmfan
 wget -P target/linux/rockchip/armv8/base-files/usr/bin/ https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux/rockchip-rk3328/base-files/usr/bin/start-rk3328-pwm-fan.sh
-# 替换默认主题为 luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
