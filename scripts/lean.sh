3170187d5397de5673545f8b0e38745e75c95924
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2022-11-02 16:02:05 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -140,3 +140,4 @@ svn co https://github.com/DHDAXCW/packages/trunk/utils/coremark customfeeds/pack
 sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
 wget -P target/linux/rockchip/armv8/base-files/etc/init.d/ https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux/rockchip-rk3328/base-files/etc/init.d/fa-rk3328-pwmfan
 wget -P target/linux/rockchip/armv8/base-files/usr/bin/ https://github.com/friendlyarm/friendlywrt/raw/master-v19.07.1/target/linux/rockchip-rk3328/base-files/usr/bin/start-rk3328-pwm-fan.sh
+echo -e "   OpenWrt firmware built on "$(date +%Y.%m.%d)"\n -------------------------------------------------------------" >> package/base-files/files/etc/banner
