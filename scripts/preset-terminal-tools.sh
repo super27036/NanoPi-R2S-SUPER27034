885c7f167408db7b8d535a54b77b31d3b9dd6bda
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2023-01-20 21:29:52 +0800

    Update preset-terminal-tools.sh

--- scripts/preset-terminal-tools.sh
+++ scripts/preset-terminal-tools.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #=================================================
+# File name: preset-terminal-tools.sh
 # System Required: Linux
 # Version: 1.0
 # Lisence: MIT
 # Author: SuLingGG
 # Blog: https://mlapp.cn
 #=================================================
-
 mkdir -p files/root
 pushd files/root
 
 ## Install oh-my-zsh
 # Clone oh-my-zsh repository
-git clone https://github.com/robbyrussell/oh-my-zsh ./.oh-my-zsh
+git clone https://github.com/ohmyzsh/ohmyzsh ./.oh-my-zsh
 
 # Install extra plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
