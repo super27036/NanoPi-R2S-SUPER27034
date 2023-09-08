11f2395df9e078e2b9b400dee8ed7447d867038a
super27036 <144255894+super27036@users.noreply.github.com>
2023-09-08 08:29:21 +0800

    Update super.sh

--- scripts/super.sh
+++ scripts/super.sh
@@ -120,6 +120,11 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 # sed -i 's/5.15/5.10/g' target/linux/rockchip/Makefile
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+# Password
+sed -i 's/root::0:0:99999:7:::/root:$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7:::/g' openwrt/package/base-files/files/etc/shadow
+# Modify default hosename
+sed -i 's/OpenWrt/SUPERouter/g' openwrt/package/base-files/files/bin/config_generate
+
 
 # 风扇脚本
 sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
