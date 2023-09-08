9a43acecdb172ca5779623c14c49691919c268e0
super27036 <144255894+super27036@users.noreply.github.com>
2023-09-08 14:38:02 +0800

    Update super.sh

--- scripts/super.sh
+++ scripts/super.sh
@@ -121,9 +121,9 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 # Password
-sed -i 's/root::0:0:99999:7:::/root:$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7:::/g' openwrt/package/base-files/files/etc/shadow
+sed -i 's/root::0:0:99999:7:::/root:$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7:::/g' package/base-files/files/etc/shadow
 # Modify default hosename
-sed -i 's/OpenWrt/SUPERouter/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/SUPERouter/g' package/base-files/files/bin/config_generate
 
 
 # 风扇脚本
