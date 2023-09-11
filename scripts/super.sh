8a46bd450bef3c7a1d1d1b0538d546f9fc7bb402
super27036 <144255894+super27036@users.noreply.github.com>
2023-09-11 11:10:36 +0800

    Update super.sh

--- scripts/super.sh
+++ scripts/super.sh
@@ -26,9 +26,9 @@ rm -rf openwrt-package/luci-app-verysync
 svn export https://github.com/QiuSimons/OpenWrt-Add/trunk/luci-app-irqbalance
 
 # Add luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
-svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
+# git clone https://github.com/xiaorouji/openwrt-passwall
+# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
+# svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
 
 # Add luci-app-ssr-plus
 git clone --depth=1 https://github.com/fw876/helloworld
