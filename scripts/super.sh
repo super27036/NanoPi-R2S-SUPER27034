ceb840247486beb463f189708f839b9fc15edca6
super27036 <144255894+super27036@users.noreply.github.com>
2023-09-14 07:49:11 +0800

    Update super.sh

--- scripts/super.sh
+++ scripts/super.sh
@@ -28,8 +28,8 @@ svn export https://github.com/QiuSimons/OpenWrt-Add/trunk/luci-app-irqbalance
 # Add luci-app-passwall
 mkdir passwall passwall-packages passwall-luci
 git clone https://github.com/xiaorouji/openwrt-passwall passwall
-git clone -b packages https://github.com/xiaorouji/openwrt-passwall passwall-packages
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall passwall-luci
+git clone https://github.com/xiaorouji/openwrt-passwall-packages passwall-packages
+# git clone -b luci https://github.com/xiaorouji/openwrt-passwall passwall-luci
 
 # git clone https://github.com/xiaorouji/openwrt-passwall
 # git clone -b packages https://github.com/xiaorouji/openwrt-passwall
