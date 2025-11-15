ad73fd7d57f5aadf5830ec775ce08969911ea399
super27036 <144255894+super27036@users.noreply.github.com>
2025-11-16 00:09:18 +0800

    Update DHDAXCW-super-2025-r3s.sh

--- scripts/DHDAXCW-super-2025-r3s.sh
+++ scripts/DHDAXCW-super-2025-r3s.sh
@@ -25,7 +25,7 @@ git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
 git clone --depth=1 https://github.com/super27035/luci-app-vssr
 
 # Add OpenClash (添加 OpenClash)
-git clone --depth=1 https://github.com/super27034/OpenClash.git
+git clone --depth=1 https://github.com/super27036/OpenClash.git
 
 # Add aliddns (添加 aliddns)
 git clone --depth=1 https://github.com/super27034/luci-app-aliddns.git
